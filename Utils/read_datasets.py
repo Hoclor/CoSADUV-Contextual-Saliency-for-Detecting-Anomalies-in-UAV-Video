18d1a55905ce546c75896e9653070ebbf21b8f8f
Simon Gokstorp <simon@gokstorp.se>
2018-11-30 10:03:07 +0000

    Change input from BGR floats to RGB ints

--- Utils/read_datasets.py
+++ Utils/read_datasets.py
@@ -10,17 +10,18 @@ import numpy as np
 import cv2
 import re
 
-def convert_SALICON_Data(dataset_directory='C:/Users/simon/Downloads/Project Datasets/SALICON', height=96, width=128):
+def convert_SALICON_Data(input_directory='C:/Users/simon/Downloads/Project Datasets/SALICON',
+    output_directory='../DSCLRCN-PyTorch/Dataset/Transformed', height=96, width=128):
     # Converts the images in the SALICON dataset to the format required by
     # the PyTorch implementation of DSCLRCN
 
     # Loop over each file in the Dataset/Untransformed folder
-    full_directory = dataset_directory + '/images'
+    full_directory = input_directory + '/images'
 
     # Create the three dictionaries
-    train_datadict = {'images': np.empty((10000, height, width, 3)), 'fix_maps': np.empty((10000, height, width))}
-    val_datadict = {'images': np.empty((5000, height, width, 3)), 'fix_maps': np.empty((5000, height, width))}
-    test_datadict = {'images': np.empty((5000, height, width, 3)), 'fix_maps': np.empty((5000, height, width))}
+    train_datadict = {'images': np.empty((10000, height, width, 3), dtype=np.uint8), 'fix_maps': np.empty((10000, height, width), dtype=np.uint8)}
+    val_datadict = {'images': np.empty((5000, height, width, 3), dtype=np.uint8), 'fix_maps': np.empty((5000, height, width), dtype=np.uint8)}
+    test_datadict = {'images': np.empty((5000, height, width, 3), dtype=np.uint8), 'fix_maps': np.empty((5000, height, width), dtype=np.uint8)}
     train_index = 0
     val_index = 0
     test_index = 0
@@ -43,8 +44,10 @@ def convert_SALICON_Data(dataset_directory='C:/Users/simon/Downloads/Project Dat
             if image_file.endswith('.jpg'):
                 # read the image
                 img = cv2.imread(full_directory + '/' + image_file)
+                # convert the image to RGB, and store values as floats in range 0-1
+                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 # Resize the image to width x height (width wide, height high)
-                resized_img = cv2.resize(img, (width, height))
+                resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
                 if 'train' in image_file:
                     # Add this to the train dict
                     train_datadict['images'][train_index] = resized_img
@@ -67,7 +70,7 @@ def convert_SALICON_Data(dataset_directory='C:/Users/simon/Downloads/Project Dat
     # Do the same for the fixation maps for training data
     print('')
     print('Reading fixation maps: Training')
-    full_directory = dataset_directory + '/fixation maps/train'
+    full_directory = input_directory + '/fixation maps/train'
     with os.scandir(full_directory) as file_iterator:
         index = 0
         count = 0
@@ -85,7 +88,7 @@ def convert_SALICON_Data(dataset_directory='C:/Users/simon/Downloads/Project Dat
                 # read the image
                 img = cv2.imread(full_directory + '/' + image_file, cv2.IMREAD_GRAYSCALE)
                 # Resize the image to width x height
-                resized_img = cv2.resize(img, (width, height))
+                resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
                 # Add this to the train dict
                 train_datadict['fix_maps'][count - 1] = resized_img
 
@@ -97,7 +100,7 @@ def convert_SALICON_Data(dataset_directory='C:/Users/simon/Downloads/Project Dat
     # Do the same for the fixation maps for validation data
     print('')
     print('Reading fixation maps: Validation')
-    full_directory = dataset_directory + '/fixation maps/val'
+    full_directory = input_directory + '/fixation maps/val'
     with os.scandir(full_directory) as file_iterator:
         count = 0
         file_names = [file_object.name for file_object in list(file_iterator)]
@@ -114,7 +117,7 @@ def convert_SALICON_Data(dataset_directory='C:/Users/simon/Downloads/Project Dat
                 # read the image
                 img = cv2.imread(full_directory + '/' + image_file, cv2.IMREAD_GRAYSCALE)
                 # Resize the image to width x height
-                resized_img = cv2.resize(img, (width, height))
+                resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
                 # Add this to the val dict
                 val_datadict['fix_maps'][count - 1] = resized_img
 
@@ -125,16 +128,20 @@ def convert_SALICON_Data(dataset_directory='C:/Users/simon/Downloads/Project Dat
 
     # Compute the mean train image
     print('Computing mean image')
-    mean_image = np.average(train_datadict['images'], axis=0)
+    mean_image = np.average(train_datadict['images'], axis=0).astype(np.uint8)
 
-    print('Writing Data to {}'.format(dataset_directory + '/Transformed/'))
+    print('Writing Data to {}'.format(output_directory))
     
-    np.save(dataset_directory + '/Transformed/mean_image.npy', mean_image)
-    with open(dataset_directory + '/Transformed/train_datadict.pickle', 'wb') as f:
+    print('Writing mean image')
+    np.save(output_directory + '/mean_image.npy', mean_image)
+    print('Writing Training data')
+    with open(output_directory + '/train_datadict.pickle', 'wb') as f:
         pickle.dump(train_datadict, f)
-    with open(dataset_directory + '/Transformed/test_datadict.pickle', 'wb') as f:
+    print('Writing Testing data')
+    with open(output_directory + '/test_datadict.pickle', 'wb') as f:
         pickle.dump(test_datadict, f)
-    with open(dataset_directory + '/Transformed/val_datadict.pickle', 'wb') as f:
+    print('Writing Validation data')
+    with open(output_directory + '/val_datadict.pickle', 'wb') as f:
         pickle.dump(val_datadict, f)
     print('Done')
 
