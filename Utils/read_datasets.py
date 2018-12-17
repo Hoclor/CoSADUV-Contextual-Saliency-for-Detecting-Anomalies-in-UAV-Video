12fccb67d6a15f2f2d9587e59c5a2fb47ee4ef59
Simon Gokstorp <simon@gokstorp.se>
2018-12-17 16:29:49 +0000

    Use TQDM for progress output

--- Utils/read_datasets.py
+++ Utils/read_datasets.py
@@ -9,6 +9,7 @@ import pickle
 import numpy as np
 import cv2
 import re
+from tqdm import tqdm
 
 def convert_SALICON_Data(input_directory='C:/Users/simon/Downloads/Project Datasets/SALICON',
     output_directory='../DSCLRCN-PyTorch/Dataset/Transformed', height=96, width=128):
@@ -16,7 +17,9 @@ def convert_SALICON_Data(input_directory='C:/Users/simon/Downloads/Project Datas
     # the PyTorch implementation of DSCLRCN
 
     # Loop over each file in the Dataset/Untransformed folder
-    full_directory = input_directory + '/images'
+    if not input_directory.endswith('/'):
+        input_directory += '/'
+    full_directory = input_directory + 'images'
 
     # Create the three dictionaries
     train_datadict = {'images': np.empty((10000, height, width, 3), dtype=np.uint8), 'fix_maps': np.empty((10000, height, width), dtype=np.uint8)}
@@ -27,104 +30,78 @@ def convert_SALICON_Data(input_directory='C:/Users/simon/Downloads/Project Datas
     test_index = 0
 
     print('')
-    print('Reading image files from {}'.format(full_directory))
+    print('Reading dataset from {}'.format(input_directory))
     
+    # Read the files and put their names in a list
     with os.scandir(full_directory) as file_iterator:
-        count = 0
         file_names = [file_object.name for file_object in list(file_iterator)]
-        file_names = sorted(file_names)
-        for image_file in file_names:
-            if count % 200 == 0:
-                # Restart the line
-                sys.stdout.write('Progress: {:3.0f}%\r'.format(count/200))
-                sys.stdout.flush()
-            count += 1
 
-            # Check if the file is .jpg
-            if image_file.endswith('.jpg'):
-                # read the image
-                img = cv2.imread(full_directory + '/' + image_file)
-                # convert the image to RGB, and store values as floats in range 0-1
-                img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-                # Resize the image to width x height (width wide, height high)
-                resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
-                if 'train' in image_file:
-                    # Add this to the train dict
-                    train_datadict['images'][train_index] = resized_img
-                    train_index += 1
-                elif 'test' in image_file:
-                    test_datadict['images'][test_index] = resized_img
-                    test_index += 1
-                elif 'val' in image_file:
-                    val_datadict['images'][val_index] = resized_img
-                    val_index += 1
-                else:
-                    # Error
-                    print('Error: unexpected file encountered ({})'.format(image_file.name))
-
-    # Restart the line
-    sys.stdout.write('Progress: 100%\r')
-    sys.stdout.flush()
     print('')
+    print('Reading image files: All')
+
+    # Sort the list
+    file_names = sorted(file_names)
+    count = 0
+    for image_file in tqdm(file_names):
+        # Check if the file is .jpg
+        if image_file.endswith('.jpg'):
+            # read the image
+            img = cv2.imread(full_directory + '/' + image_file)
+            # convert the image to RGB, and store values as floats in range 0-1
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            # Resize the image to width x height (width wide, height high)
+            resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
+            if 'train' in image_file:
+                # Add this to the train dict
+                train_datadict['images'][train_index] = resized_img
+                train_index += 1
+            elif 'test' in image_file:
+                test_datadict['images'][test_index] = resized_img
+                test_index += 1
+            elif 'val' in image_file:
+                val_datadict['images'][val_index] = resized_img
+                val_index += 1
+            else:
+                # Error
+                print('Error: unexpected file encountered ({})'.format(image_file.name))
 
     # Do the same for the fixation maps for training data
     print('')
     print('Reading fixation maps: Training')
     full_directory = input_directory + '/fixation maps/train'
     with os.scandir(full_directory) as file_iterator:
-        index = 0
-        count = 0
         file_names = [file_object.name for file_object in list(file_iterator)]
-        file_names = sorted(file_names)
-        for image_file in file_names:
-            if count % 100 == 0:
-                # Restart the line
-                sys.stdout.write('Progress: {:3.0f}%\r'.format(count/100))
-                sys.stdout.flush()
-            count += 1
 
-            # Check if the file is .png
-            if image_file.endswith('.png'):
-                # read the image
-                img = cv2.imread(full_directory + '/' + image_file, cv2.IMREAD_GRAYSCALE)
-                # Resize the image to width x height
-                resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
-                # Add this to the train dict
-                train_datadict['fix_maps'][count - 1] = resized_img
-
-    # Restart the line
-    sys.stdout.write('Progress: 100%\r')
-    sys.stdout.flush()
-    print('')
+    file_names = sorted(file_names)
+    count = 0
+    for image_file in tqdm(file_names):
+        # Check if the file is .png
+        if image_file.endswith('.png'):
+            # read the image
+            img = cv2.imread(full_directory + '/' + image_file, cv2.IMREAD_GRAYSCALE)
+            # Resize the image to width x height
+            resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
+            # Add this to the train dict
+            train_datadict['fix_maps'][count - 1] = resized_img
 
     # Do the same for the fixation maps for validation data
     print('')
     print('Reading fixation maps: Validation')
     full_directory = input_directory + '/fixation maps/val'
     with os.scandir(full_directory) as file_iterator:
-        count = 0
         file_names = [file_object.name for file_object in list(file_iterator)]
-        file_names = sorted(file_names)
-        for image_file in file_names:
-            if count % 50 == 0:
-                # Restart the line
-                sys.stdout.write('Progress: {:3.0f}%\r'.format(count/50))
-                sys.stdout.flush()
-            count += 1
 
-            # Check if the file is .png
-            if image_file.endswith('.png'):
-                # read the image
-                img = cv2.imread(full_directory + '/' + image_file, cv2.IMREAD_GRAYSCALE)
-                # Resize the image to width x height
-                resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
-                # Add this to the val dict
-                val_datadict['fix_maps'][count - 1] = resized_img
-
-    # Restart the line
-    sys.stdout.write('Progress: 100%\r')
-    sys.stdout.flush()
-    print('')
+    file_names = sorted(file_names)
+    count = 0
+    for image_file in tqdm(file_names):
+        # Check if the file is .png
+        if image_file.endswith('.png'):
+            # read the image
+            img = cv2.imread(full_directory + '/' + image_file, cv2.IMREAD_GRAYSCALE)
+            # Resize the image to width x height
+            resized_img = cv2.resize(img, (width, height)).astype(np.uint8)
+            # Add this to the val dict
+            val_datadict['fix_maps'][count - 1] = resized_img
 
     # Compute the mean train image
     print('Computing mean image')
@@ -145,5 +122,6 @@ def convert_SALICON_Data(input_directory='C:/Users/simon/Downloads/Project Datas
         pickle.dump(val_datadict, f)
     print('Done')
 
-
-convert_SALICON_Data(height=96, width=128)
+# Execute this function with h=96, w=128 if this file is the main file
+if __name__ == "__main__":
+    convert_SALICON_Data(height=96, width=128)
