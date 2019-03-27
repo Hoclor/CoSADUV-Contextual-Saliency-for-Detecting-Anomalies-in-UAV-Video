2abd0a34d2dfcea947b2acedbebc91ff79d30dd7
Simon Gokstorp <simon@gokstorp.se>
2019-03-27 16:49:26 +0000

    Implement ground truth drawing function

--- Utils/uav123_annotation_converter.py
+++ Utils/uav123_annotation_converter.py
@@ -78,14 +78,16 @@ def draw_annotations(dataset_folder, sequence_name, display=False):
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         
-        frame_count += 1
+def draw_groundtruth(dataset_folder, sequence_name, display=False):
+    # Get the sequnce folder and annotations folder
+    sequence_folder = os.path.join(dataset_folder, 'data_seq', 'UAV123', sequence_name)
+    annotation_file = os.path.join(dataset_folder, 'anno', 'UAV123', sequence_name + '.txt')
+    target_folder   = os.path.join(dataset_folder, 'ground_truth', 'UAV123', sequence_name)
+    # Check if the target_folder exists
+    if not os.path.exists(target_folder):
+        # Create the target_folder
+        os.makedirs(target_folder)
 
-    # Release everything
-    cap.release()
-    out.release()
-    cv2.destroyAllWindows()
-
-def draw_groundtruth(sequence_name, sequence_folder, annotation_file, display=False):
     # Get a list of frames in sequence_folder
     if os.name == 'posix':
         # Unix
@@ -101,8 +103,8 @@ def draw_groundtruth(sequence_name, sequence_folder, annotation_file, display=Fa
         annotations = f.readlines()
     # Convert each line from a string to a list with 4 numbers
     def process_line(line):
-        ret = line.split(','),
-        ret = list(map(int, ret))
+        ret = line.strip().split(',')
+        ret = list(map(lambda n : int(n) if n != 'NaN' else -1, ret))
         return ret
     annotations = list(map(process_line, annotations))
     
@@ -111,85 +113,44 @@ def draw_groundtruth(sequence_name, sequence_folder, annotation_file, display=Fa
     fourcc = cv2.VideoWriter_fourcc(*'DIVX')
     framerate = 30
     (width, height) = (1280, 720)
-    out = cv2.VideoWriter(sequence_name + '_groundtruth.avi', fourcc, framerate, (width, height), 0)
+    out = cv2.VideoWriter(os.path.join(target_folder, sequence_name + '.avi'), fourcc, framerate, (width, height), 0)
 
     blank_frame = np.zeros((height, width), dtype=np.uint8)
 
-    for frame_count, frame_name in tqdm(enumerate(frames)):
+    last_time = time.time()
+    for frame_count, frame_name in enumerate(tqdm(frames)):
         # Copy a new blank frame
         frame = np.copy(blank_frame)
 
-        # Get the next line of the annotations file
-        for track in tracks:
-            # Check that this track has any box nodes left
-            if(len(track) > 0):
-                # Since the nodes are sorted by frame number, we only have to check the first one
-                box = track[0]
-                if(int(box.attrib['frame']) == frame_count):
-                    # Draw the rectangle described by this 'box' node on this frame
-                    # Cast the coordinates to floats, then to ints, as the cv2.rectangle function cannot handle float pixel values
-                    # And int(str) cannot handle float strings
-                    x_tl = int(float(box.attrib['xtl']))
-                    y_tl = int(float(box.attrib['ytl']))
-                    x_br = int(float(box.attrib['xbr']))
-                    y_br = int(float(box.attrib['ybr']))
-                    cv2.rectangle(frame, (x_tl, y_tl), (x_br, y_br), 255, cv2.FILLED)
-                    # delete this box from the track, so we can keep only checking the first box in the future
-                    track.remove(box)
+        # Get the corresponding annotation
+        annotation = annotations[frame_count]
         
-        # Write the frame with boxes
-        # Convert to BGR so video can be properly saved
-        # frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-        out.write(frame)
-        # Display the resulting frame
-        if display:
-            cv2.imshow('frame',frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        
-        frame_count += 1
-    
-    # Keep going, as the frame count is not necessarily accurate so we might not be done
-    while(True):
-        # Copy a new blank frame
-        frame = np.copy(blank_frame)
-        # Read the next frame but discard it, to check if the video is done yet
-        ret, og_frame = cap.read()
-        if not ret:
-            # Video is done, so break out of the loop
-            break
-
-        # Loop over the track objects. For all that have an annotation for this frame, draw a corresponding rectangle with a colour from the colours list
-        for track in tracks:
-            # Check that this track has any box nodes left
-            if(len(track) > 0):
-                # Since the nodes are sorted by frame number, we only have to check the first one
-                box = track[0]
-                if(int(box.attrib['frame']) == frame_count):
-                    # Draw the rectangle described by this 'box' node on this frame
-                    # Cast the coordinates to floats, then to ints, as the cv2.rectangle function cannot handle float pixel values
-                    # And int(str) cannot handle float strings
-                    x_tl = int(float(box.attrib['xtl']))
-                    y_tl = int(float(box.attrib['ytl']))
-                    x_br = int(float(box.attrib['xbr']))
-                    y_br = int(float(box.attrib['ybr']))
+        # Only draw an annotiation if it exists - i.e. annotation is not -1
+        if all(a != -1 for a in annotation):
+            # Draw the rectangle as a filled in white box
+            x_tl = annotation[0]
+            y_tl = annotation[1]
+            x_br = x_tl + annotation[2]
+            y_br = y_tl + annotation[3]
                     cv2.rectangle(frame, (x_tl, y_tl), (x_br, y_br), 255, cv2.FILLED)
-                    # delete this box from the track, so we can keep only checking the first box in the future
-                    track.remove(box)
         
         # Write the frame with boxes 
         out.write(frame)
+        # Also save the frame individually
+        cv2.imwrite(os.path.join(target_folder, frame_name[:-4] + '.png'), frame)
         # Display the resulting frame
         if display:
+            # Force the function to run at the framerate of the video
+            while time.time() - last_time < ((1/framerate)/1.025): # Enforce time passed as slightly less than 1/framerate, as displaying the output takes some time as well
+                pass
+            last_time = time.time()
             cv2.imshow('frame',frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
         
-        frame_count += 1
 
 
     # Release everything
-    cap.release()
     out.release()
     cv2.destroyAllWindows()
 
